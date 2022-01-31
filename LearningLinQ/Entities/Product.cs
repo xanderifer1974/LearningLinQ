a41bf6332f1484e23a9516c474bb964a47a15841
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2022-01-31 18:28:09 -0300

    Iniciando o projeto LearningLINQ

---
+++ LearningLinQ/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace LearningLinQ.Entities
+{
+    class Product : IComparable<Product>
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(Product other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
